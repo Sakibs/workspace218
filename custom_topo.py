6942680bdaf352471261570dea0d20e669440b03
Sakib <sakibs8@gmail.com>
2014-11-02 17:46:21 -0800

    Added ryu examples

--- custom_topo.py
+++ custom_topo.py
@@ -6,21 +6,21 @@ from mininet.log import setLogLevel, info
 def setup():
 	net = Mininet(controller=Controller)
 
-	info('**** Adding Controller ****')
+	info('**** Adding Controller ****\n')
 	net.addController( 'c0' )
 
-	info('**** Setting up Hosts ****')
-	h1 = net.addHost('h1', ip=10.0.0.10)
-	h2 = net.addHost('h2', ip=10.0.0.20)
-	h3 = net.addHost('h3', ip=10.0.0.30)
-	h4 = net.addHost('h4', ip=10.0.0.40)
+	info('**** Setting up Hosts ****\n')
+	h1 = net.addHost('h1', ip='10.0.0.10')
+	h2 = net.addHost('h2', ip='10.0.0.20')
+	h3 = net.addHost('h3', ip='10.0.0.30')
+	h4 = net.addHost('h4', ip='10.0.0.40')
 
-	info('**** Setting up Switches ****')
+	info('**** Setting up Switches ****\n')
 	s1 = net.addSwitch('s1')
 	s2 = net.addSwitch('s2')
 	s3 = net.addSwitch('s3')
 
-	info('**** Settings Links ****')
+	info('**** Settings Links ****\n')
 	net.addLink(s1, s2)
 	net.addLink(s2, s3)
 	net.addLink(h1, s1)
@@ -28,17 +28,17 @@ def setup():
 	net.addLink(h3, s3)
 	net.addLink(h4, s3)
 
-	info('**** Starting Ntwk ****')
+	info('**** Starting Ntwk ****\n')
 	net.start()
 
-	info('**** Running CLI ****')
+	info('**** Running CLI ****\n')
 	CLI( net )
 
-	info('**** Stopping Ntwk ****')
+	info('**** Stopping Ntwk ****\n')
 	net.stop()
 
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
-    emptyNet()
+    setup()
 
