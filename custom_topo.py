0c8461c638abbc413fdbf57a920272f75a5aa5cb
Sakib <sakibs8@gmail.com>
2014-11-03 22:16:55 -0800

    modeled multipath topology

--- custom_topo.py
+++ custom_topo.py
@@ -1,13 +1,13 @@
 from mininet.net import Mininet
-from mininet.node import Controller
+from mininet.node import OVSSwitch, RemoteController, Controller
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 
 def setup():
-	net = Mininet(controller=Controller)
+	net = Mininet(controller=RemoteController, switch=OVSSwitch)
 
-	info('**** Adding Controller ****\n')
-	net.addController( 'c0' )
+	info('**** Adding Remote Controller ****\n')
+	net.addController(name='c1', defaultIP='127.0.0.1', port=6633 )
 
 	info('**** Setting up Hosts ****\n')
 	h1 = net.addHost('h1', ip='10.0.0.10')
@@ -19,14 +19,18 @@ def setup():
 	s1 = net.addSwitch('s1')
 	s2 = net.addSwitch('s2')
 	s3 = net.addSwitch('s3')
+	s4 = net.addSwitch('s4')
 
 	info('**** Settings Links ****\n')
+	net.addLink(h1, s1)
+	net.addLink(h2, s2)
+	net.addLink(h3, s3)
+	net.addLink(h4, s4)
+	
 	net.addLink(s1, s2)
 	net.addLink(s2, s3)
-	net.addLink(h1, s1)
-	net.addLink(h2, s1)
-	net.addLink(h3, s3)
-	net.addLink(h4, s3)
+	net.addLink(s3, s4)
+	net.addLink(s4, s1)
 
 	info('**** Starting Ntwk ****\n')
 	net.start()
